5b6fd1621f5d6a9d320ac9819d460c77f28b2765
Daniil Gubin <ezickk@icloud.com>
2025-05-04 14:46:46 +0300

    Написал tcp сервер для бд
    Написал простой tcp сервер чтобы отправлять запросы бд

--- backend_server/main_db.cpp
+++ backend_server/main_db.cpp
@@ -1,32 +1,18 @@
 #include <QCoreApplication>
-#include <QDebug>
 #include "databasemanager.h"
+#include "dbtcpserver.h"
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    // Попытка инициализации БД
+    // Инициализируем БД
     if (!DatabaseManager::instance().init("Test.db")) {
-        qCritical() << "❌ Не удалось открыть или создать БД";
         return -1;
     }
-    qDebug() << "✅ БД инициализирована";
 
-    // Создадим тестовую таблицу (если ещё нет)
-    QString result = DatabaseManager::instance().execute(
-        "CREATE TABLE IF NOT EXISTS TestTable("
-        "id INTEGER PRIMARY KEY, "
-        "name TEXT"
-        ");"
-        );
-    qDebug() << "CREATE TABLE result:" << result;
+    // Запускаем DB-сервер
+    DbTcpServer server(33333);
 
-    // Выведем все таблицы в БД
-    result = DatabaseManager::instance().execute(
-        "SELECT name FROM sqlite_master WHERE type='table';"
-        );
-    qDebug() << "Список таблиц:\n" << result;
-
-    return 0;
+    return a.exec();
 }
