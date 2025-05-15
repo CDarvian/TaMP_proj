a89a55d7fe4a13d2d685446b8ef0f513956be357
Daniil Gubin <ezickk@icloud.com>
2025-05-15 18:23:41 +0300

    Переписал echo-server (несколько клиентов)
    Реализовал поддержку одновременных подключений нескольких клиентов через буфер

--- backend_server/mytcpserver.cpp
+++ backend_server/mytcpserver.cpp
@@ -2,61 +2,88 @@
 #include "functions_to_server.h"
 #include <QDebug>
 
-MyTcpServer::MyTcpServer(QObject *parent) : QObject(parent) {
-    mTcpServer = new QTcpServer(this);
-    connect(mTcpServer, &QTcpServer::newConnection,
-            this, &MyTcpServer::slotNewConnection);
+MyTcpServer::MyTcpServer(quint16 port, QObject *parent)
+    : QObject(parent),
+    m_server(new QTcpServer(this))
+{
+    connect(m_server, &QTcpServer::newConnection,
+            this, &MyTcpServer::onNewConnection);
 
-    if (!mTcpServer->listen(QHostAddress::Any, 33333)) {
-        qDebug() << "Server is not started";
+    if (!m_server->listen(QHostAddress::Any, port)) {
+        qCritical() << "Не удалось запустить сервер на порту" << port;
     } else {
-        qDebug() << "Server is started on port 33333";
+        qDebug() << "Сервер запущен на порту" << port;
     }
 }
 
 MyTcpServer::~MyTcpServer() {
-    mTcpServer->close();
+    m_server->close();
+    // сокеты удалятся в onClientDisconnected
 }
 
-void MyTcpServer::slotNewConnection() {
-    mTcpSocket = mTcpServer->nextPendingConnection();
-    qDebug() << "Client connected from" << mTcpSocket->peerAddress().toString();
-    connect(mTcpSocket, &QTcpSocket::readyRead, this, &MyTcpServer::slotServerRead);
-    connect(mTcpSocket, &QTcpSocket::disconnected, this, &MyTcpServer::slotClientDisconnected);
-}
+void MyTcpServer::onNewConnection() {
+    while (m_server->hasPendingConnections()) {
+        QTcpSocket *client = m_server->nextPendingConnection();
+        m_clients.append(client);
+        m_buffers[client].clear();
+        qDebug() << "Клиент подключился:"
+                 << client->peerAddress().toString()
+                 << ":" << client->peerPort();
 
-void MyTcpServer::slotServerRead() {
-    // Читаем всё, копим до разделителя '\n'
-    while (mTcpSocket->bytesAvailable() > 0) {
-        mBuffer += QString::fromUtf8(mTcpSocket->readAll());
-        int idx;
-        while ((idx = mBuffer.indexOf('\n')) != -1) {
-            QString line = mBuffer.left(idx).trimmed();
-            mBuffer.remove(0, idx + 1);
-
-            // Примитивный парсер: команды вида "CMD:payload"
-            QString response;
-            if (line.startsWith("LOGIN:")) {
-                response = handleLogin(line.mid(QString("LOGIN:").length()));
-            }
-            else if (line.startsWith("REGISTER:")) {
-                response = handleRegister(line.mid(QString("REGISTER:").length()));
-            }
-            else if (line.startsWith("STATS:")) {
-                response = handleStats(line.mid(QString("STATS:").length()));
-            }
-            else {
-                // эхо на всё остальное
-                response = "ECHO: " + line;
-            }
-
-            // Отправляем с '\r\n'
-            mTcpSocket->write(response.toUtf8() + "\r\n");
-        }
+        connect(client, &QTcpSocket::readyRead,
+                this, &MyTcpServer::onReadyRead);
+        connect(client, &QTcpSocket::disconnected,
+                this, &MyTcpServer::onClientDisconnected);
     }
 }
 
-void MyTcpServer::slotClientDisconnected() {
-    qDebug() << "Client disconnected";
-    mTcpSocket->close();
+void MyTcpServer::onReadyRead() {
+    // Узнаём, какой клиент прислал данные
+    QTcpSocket *client = qobject_cast<QTcpSocket*>(sender());
+    if (!client) return;
+
+    // Накопим в буфер
+    m_buffers[client] += client->readAll();
+
+    int idx;
+    // Разбираем по строкам (\n)
+    while ((idx = m_buffers[client].indexOf('\n')) != -1) {
+        QByteArray raw = m_buffers[client].left(idx).trimmed();
+        m_buffers[client].remove(0, idx + 1);
+
+        QString line = QString::fromUtf8(raw);
+        qDebug() << "Получено от" << client->peerAddress().toString()
+                 << ":" << line;
+
+        // Обработка команд
+        QString response;
+        if (line.startsWith("LOGIN:")) {
+            response = handleLogin(line.mid(QString("LOGIN:").length()));
+        }
+        else if (line.startsWith("REGISTER:")) {
+            response = handleRegister(line.mid(QString("REGISTER:").length()));
+        }
+        else if (line.startsWith("STATS:")) {
+            response = handleStats(line.mid(QString("STATS:").length()));
+        }
+        else {
+            // Обычное эхо
+            response = "ECHO: " + line;
+        }
+
+        // Отправляем ответ с CRLF
+        client->write(response.toUtf8() + "\r\n");
+        client->flush();
+    }
+}
+
+void MyTcpServer::onClientDisconnected() {
+    QTcpSocket *client = qobject_cast<QTcpSocket*>(sender());
+    if (!client) return;
+
+    qDebug() << "Клиент отключился:"
+             << client->peerAddress().toString();
+    m_clients.removeAll(client);
+    m_buffers.remove(client);
+    client->deleteLater();
 }
