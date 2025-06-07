09809b35eb29d94c1adbcf4647c68876dacbdcaa
ssofpv <s_makarycheva.18@mail.ru>
2025-06-07 17:00:23 +0300

    Add files via upload

---
+++ Doxigen/frontend_client/html/navtreeindex0.js
@@ -0,0 +1,17 @@
+var NAVTREEINDEX0 =
+{
+"annotated.html":[0,0],
+"class_main_window.html":[0,0,0],
+"class_main_window.html#a1f7d2c56410fc1bfa135bb907d43313f":[0,0,0,1],
+"class_main_window.html#a996c5a2b6f77944776856f08ec30858d":[0,0,0,0],
+"classes.html":[0,1],
+"files.html":[1,0],
+"functions.html":[0,3,0],
+"functions_func.html":[0,3,1],
+"hierarchy.html":[0,2],
+"index.html":[],
+"mainwindow_8cpp.html":[1,0,0],
+"mainwindow_8h.html":[1,0,1],
+"mainwindow_8h_source.html":[1,0,1],
+"pages.html":[]
+};
